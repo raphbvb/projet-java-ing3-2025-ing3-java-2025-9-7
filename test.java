541a75ff798c1afd30f19ef863bc09c60d163a65
Simon <129516845+Urazikk@users.noreply.github.com>
2025-04-20 22:11:22 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+public class Vide {
+    public void vide() {
+        // commentaires inutiles
+        // commentaire 1
+        // commentaire 2
+        // commentaire 3
+        // commentaire 4
+        // commentaire 5
+        // commentaire 6
+        // commentaire 7
+        // commentaire 8
+        // commentaire 9
+        // commentaire 10
+    }
+}
