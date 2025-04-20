50cb7e687a5f36aa327f040b0b908de00b770acb
Simon <129516845+Urazikk@users.noreply.github.com>
2025-04-20 22:16:02 +0200

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,40 @@
+public class Vide {
+    public void vide() {
+        String message1 = "Ceci est un message numéro 1";
+        String message2 = "Ceci est un message numéro 2";
+        String message3 = "Ceci est un message numéro 3";
+        String message4 = "Ceci est un message numéro 4";
+        String message5 = "Ceci est un message numéro 5";
+        String message6 = "Ceci est un message numéro 6";
+        String message7 = "Ceci est un message numéro 7";
+        String message8 = "Ceci est un message numéro 8";
+        String message9 = "Ceci est un message numéro 9";
+        String message10 = "Ceci est un message numéro 10";
+        String message11 = "Ceci est un message numéro 11";
+        String message12 = "Ceci est un message numéro 12";
+        String message13 = "Ceci est un message numéro 13";
+        String message14 = "Ceci est un message numéro 14";
+        String message15 = "Ceci est un message numéro 15";
+        String message16 = "Ceci est un message numéro 16";
+        String message17 = "Ceci est un message numéro 17";
+        String message18 = "Ceci est un message numéro 18";
+        String message19 = "Ceci est un message numéro 19";
+        String message20 = "Ceci est un message numéro 20";
+        String message21 = "Ceci est un message numéro 21";
+        String message22 = "Ceci est un message numéro 22";
+        String message23 = "Ceci est un message numéro 23";
+        String message24 = "Ceci est un message numéro 24";
+        String message25 = "Ceci est un message numéro 25";
+        String message26 = "Ceci est un message numéro 26";
+        String message27 = "Ceci est un message numéro 27";
+        String message28 = "Ceci est un message numéro 28";
+        String message29 = "Ceci est un message numéro 29";
+        String message30 = "Ceci est un message numéro 30";
+        String message31 = "Ceci est un message numéro 31";
+        String message32 = "Ceci est un message numéro 32";
+        String message33 = "Ceci est un message numéro 33";
+        String message34 = "Ceci est un message numéro 34";
+        String message35 = "Ceci est un message numéro 35";
+        String message36 = "Ceci est un message numéro 36
+    }
+    }
